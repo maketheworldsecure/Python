36e525a6a792cf5942f80750b10c1e7a92d3aba1
Deepak Krishnan P.R <deepakkrishnanpr@gmail.com>
2020-03-28 03:37:05 -0400

    Add files via upload
    Program to Print string in particular format.

---
+++ 1Stringformat.py
@@ -0,0 +1,11 @@
+#! usr/bin/env python
+
+import sys
+
+print("______________________________________________________________________")
+print("Day 1 : Format Strings")
+print("______________________________________________________________________")
+print("Python Version :")
+print(sys.version)
+print("======================================================================")
+print("Twinkle, twinkle, little star , \n\tHow I wonder what you are! \n\t\tUp above the world so high,\n\t\tLike a diamond in the sky.winkle,\nTwinkle, little star , \n\tHow I wonder what you are!")
